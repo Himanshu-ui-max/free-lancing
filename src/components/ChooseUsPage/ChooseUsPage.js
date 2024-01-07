13b578ef099feb0ffeedb9483438bd9e5a5e7f76
aryan-narang <aryannarang014@gmail.com>
2024-01-07 13:28:37 +0530

    choose us

--- src/components/ChooseUsPage/ChooseUsPage.js
+++ src/components/ChooseUsPage/ChooseUsPage.js
@@ -1,11 +1,84 @@
-import React from 'react'
-import './ChooseUsPage.css'
+import React from "react";
+import "./ChooseUsPage.css";
 const ChooseUsPage = () => {
   return (
-    <div className='chooseus' id='chooseUs'>
-      this is choose us page
+    <div className="chooseus" id="chooseUs">
+      <div
+        className="d-flex justify-content-center"
+        style={{ fontSize: "40px" ,marginBottom:"60px"}}
+      >
+        Why Choose Us
+      </div>
+      <div className="d-flex justify-content-evenly">
+        <div style={{ alignItems: "center", justifyContent: "center", width:"150px", height:"200px", textAlign:"center"}}>
+         
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100"
+            height="100"
+            viewBox="0 0 196 196"
+            fill="none"
+            style={{marginBottom:"15px"}}
+          >
+            <circle cx="98" cy="98" r="98" fill="#E4E4E4" />
+          </svg>
+          <p style={{ fontSize: "12px", }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididu{" "}
+          </p>
+        </div>
+        
+        <div style={{ alignItems: "center", justifyContent: "center", width:"150px", height:"200px", textAlign:"center" }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100"
+            height="100"
+            viewBox="0 0 196 196"
+            fill="none"
+            style={{marginBottom:"15px"}}
+          >
+            <circle cx="98" cy="98" r="98" fill="#E4E4E4" />
+          </svg>
+          <p style={{ fontSize: "12px" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididu{" "}
+          </p>
+        </div>
+        <div style={{ alignItems: "center", justifyContent: "center", width:"150px", height:"200px", textAlign:"center" }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100"
+            height="100"
+            viewBox="0 0 196 196"
+            fill="none"
+            style={{marginBottom:"15px"}}
+          >
+            <circle cx="98" cy="98" r="98" fill="#E4E4E4" />
+          </svg>
+          <p style={{ fontSize: "12px" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididu{" "}
+          </p>
+        </div>
+        <div style={{ alignItems: "center", justifyContent: "center", width:"150px", height:"200px", textAlign:"center" }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100"
+            height="100"
+            viewBox="0 0 196 196"
+            fill="none"
+            style={{marginBottom:"15px"}}
+          >
+            <circle cx="98" cy="98" r="98" fill="#E4E4E4" />
+          </svg>
+          <p style={{ fontSize: "12px" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididu{" "}
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChooseUsPage
+export default ChooseUsPage;
