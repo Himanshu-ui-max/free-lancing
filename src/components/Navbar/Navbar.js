6e89d7ae8a66ead5383174b94421c1db7251442a
Himanshu-ui-max <himanshusharma234567@gmail.com>
2024-01-07 15:52:42 +0530

    navbar problem fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,10 +5,13 @@ import { HashLink } from 'react-router-hash-link'
 const Navbar = () => {
   return (
   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="d-flex justify-content-between container-fluid">
+  <div className="d-flex justify-content-between container-fluid" style={{'zIndex':'1','backgroundColor':'white','display':'flex','justifyContent':'space-between'}}>
     <a className="navbar-brand" href="/" style={{'display':'block'}}><img src={logo}/></a>
-    <div>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{'flexDirection':'row-reverse'}}>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0" id='navitems'>
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
         </li>
