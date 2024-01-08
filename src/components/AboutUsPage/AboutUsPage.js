4b9f4c471838ef3c8f3b77091363c6dbc29e2013
shubhzieee <130304179+shubhzieee@users.noreply.github.com>
2024-01-08 13:22:58 +0530

    .

--- src/components/AboutUsPage/AboutUsPage.js
+++ src/components/AboutUsPage/AboutUsPage.js
@@ -6,16 +6,14 @@ const AboutUsPage = () => {
     <section id='aboutus' className='aboutus'>
       <div className='d-flex justify-content-center'>
       <div className="abtimage d-flex justify-content-center" style={{width:'40%'}}>
-      <img style={{ height:'100%',width:'70%',borderRadius: '6px'}} src="https://via.placeholder.com/578x653"/>
+      <img style={{ height:'100%',width:'70%',borderRadius: '6px'}} src="https://via.placeholder.com/578x653" alt=''/>
       </div>
       <div className="abttext" style={{width: "60%", marginRight:'5%'}}>
-      <div style={{textAlign: "left", color: "black", fontSize: "50px", fontFamily: "Montserrat", fontWeight: "700", letterSpacing: "0.50px", wordWrap: "break-word"}}>About us</div>
-        <div style={{textAlign: "justify", color: "#666666", fontSize: "31px", fontFamily: "Montserrat", fontWeight: "500", letterSpacing: "0.31px", wordWrap: "break-word",marginTop:'3%'}}>
+      <div style={{textAlign: "left", color: "black", fontSize: "2.7rem", fontFamily: "Montserrat", fontWeight: "700", letterSpacing: "0.50px", wordWrap: "break-word"}}>About us</div>
+        <div style={{textAlign: "justify", color: "#666666", fontSize: "1.6rem", fontFamily: "Montserrat", fontWeight: "500", letterSpacing: "0.31px", wordWrap: "break-word",marginTop:'2%',marginBottom:'2.75%'}}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
         </div>
-        <a href="" style={{textDecoration:'none'}}>
-        <div style={{ marginTop:'5%',height: '10%', width:'40%',background: '#A26B39', borderRadius: '20px',color: 'white', fontSize: '31px', fontFamily: 'Montserrat', fontWeight: '600', wordWrap: 'break-word',textAlign: "center"}}>Company Profile</div>
-        </a>
+        <button className='BT'><a className='atag' href='https://online.fliphtml5.com/gqcm/ypfz/#p=3' target='_blank' rel="noreferrer">Company Profile</a></button>
         </div>
         </div>
     </section>
