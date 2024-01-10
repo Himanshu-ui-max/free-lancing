2578aad694e9c28a612ab634d6be815b62a8b9dc
Himanshu-ui-max <himanshusharma234567@gmail.com>
2024-01-10 23:11:16 +0530

    product details logic initiated

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProductDetails.css'
+import details from "./Details.json"
+import { useParams } from 'react-router-dom'
 const ProductDetails = () => {
+  const {name} = useParams();
+  const findpos=(details)=>{
+    for (let index = 0; index < details.length; index++) {
+      const element = details[index];
+      if(element.id === name){
+        return index;
+      }
+    }
+  }
+  const [productDetails, setProductDetails] = useState({});
+  useEffect(() => {
+    let i = findpos(details);
+    setProductDetails({...(details[i])});
+    console.log(productDetails)
+  }, [name]);
   return (
     <div className='productDetails'>
       this is product details
