c02cc58059b6753c52331694ff04c0673351eb99
Himanshu-ui-max <himanshusharma234567@gmail.com>
2024-02-12 18:45:47 +0530

    .

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { HashLink } from 'react-router-hash-link'
 const Navbar = () => {
   return (
   <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{background : "white"}}>
-  <div className="d-flex justify-content-between container-fluid" style={{zIndex:'1',display:'flex',justifyContent:'space-between',alignItems : 'center'}}>
+  <div className="d-flex justify-content-between container-fluid" style={{zIndex:'1',display:'flex',justifyContent:'space-between',alignItems : 'center',backgroundColor : "white"}}>
     <a className="navbar-brand" href="/" style={{'display':'block'}}><img src={logo} alt='' className='logoimage'/></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
