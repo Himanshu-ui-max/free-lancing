1fdffd479454d36187ff82ba09179dea40b41993
Himanshu-ui-max <himanshusharma234567@gmail.com>
2024-02-20 15:20:18 +0530

    .

--- src/components/HomePage/Homepage.js
+++ src/components/HomePage/Homepage.js
@@ -6,7 +6,6 @@ const Homepage = () => {
       <div className='homecontain'>
 
       <div className='hometext'>
-      MATERIAL HANDLING CRATES AND FLEXIBLE PROTECTIVE PACKAGING.
       </div>
       <button className='BT'><a className='atag' href='https://online.fliphtml5.com/gqcm/ypfz/#p=3' target='_blank' rel="noreferrer">Our Catalogue</a></button>
       </div>
