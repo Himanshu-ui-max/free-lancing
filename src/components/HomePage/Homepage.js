555a2afbd9fffb0acbf3eea4375253c53c454f5e
Himanshu-ui-max <himanshusharma234567@gmail.com>
2024-02-21 12:19:19 +0530

    .

--- src/components/HomePage/Homepage.js
+++ src/components/HomePage/Homepage.js
@@ -6,8 +6,12 @@ const Homepage = () => {
       <div className='homecontain'>
 
       <div className='hometext'>
+        <span className='impWord'></span>
       </div>
+      <div className='buttonDiv'>
+
       <button className='BT'><a className='atag' href='https://online.fliphtml5.com/gqcm/ypfz/#p=3' target='_blank' rel="noreferrer">Our Catalogue</a></button>
+      </div>
       </div>
     </div>
   )
