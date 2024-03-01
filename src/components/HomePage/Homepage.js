2eee621d5a6fd9c18e66cdf892de8c07973159ae
Himanshu-ui-max <himanshusharma234567@gmail.com>
2024-03-01 17:11:13 +0530

    .

--- src/components/HomePage/Homepage.js
+++ src/components/HomePage/Homepage.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import './HomePage.css'
+import { FaFacebook } from 'react-icons/fa';
+import { FaYoutube } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
 const Homepage = () => {
   return (
     <div className='homePage' id='homePage'>
-      <div className='homecontain'>
 
-      <div className='hometext'>
-        <span className='impWord'></span>
-      </div>
-      <div className='buttonDiv'>
+      <div className='homecontain' style={{display : "flex", justifyContent : "space-between"}}>
+        <div>
+          <div className='hometext'>
+            <span className='impWord'></span>
+          </div>
+          <div className='buttonDiv'>
 
-      <button className='BT'><a className='atag' href='https://online.fliphtml5.com/gqcm/ypfz/#p=3' target='_blank' rel="noreferrer">Our Catalogue</a></button>
-      </div>
+            <button className='BT'><a className='atag' href='https://online.fliphtml5.com/gqcm/ypfz/#p=3' target='_blank' rel="noreferrer">Our Catalogue</a></button>
+          </div>
+        </div>
+        <div className='socialDiv' style={{ display: "flex", flexDirection: "column", alignItems : "center", justifyContent : "center" }}>
+          <a href='https://www.facebook.com' target='_blank' rel="noreferrer" ><FaFacebook style={{ height: "47px", width: "47px", margin : "10px" }} /></a>
+          <a href='https://www.facebook.com' target='_blank' rel="noreferrer" ><FaYoutube style={{ height: "47px", width: "47px", margin : "10px" }} /></a>
+          <a href='https://www.facebook.com' target='_blank' rel="noreferrer" ><FaTwitter style={{ height: "47px", width: "47px", margin : "10px" }} /></a>
+        </div>
       </div>
     </div>
   )
