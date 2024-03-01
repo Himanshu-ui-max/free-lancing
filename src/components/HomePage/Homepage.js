3d8398d54e32545d8d0fc099871c1c8a3eac7bce
Himanshu-ui-max <himanshusharma234567@gmail.com>
2024-03-01 22:47:26 +0530

    .

--- src/components/HomePage/Homepage.js
+++ src/components/HomePage/Homepage.js
@@ -16,9 +16,9 @@ const Homepage = () => {
           </div>
         </div>
         <div className='socialDiv' style={{ display: "flex", flexDirection: "column", alignItems : "center", justifyContent : "center" }}>
-          <a href='https://www.facebook.com/profile.php?id=100079658515735' target='_blank' rel="noreferrer" ><FaFacebook style={{ height: "47px", width: "47px", margin : "10px" }} /></a>
-          <a href='https://www.instagram.com' target='_blank' rel="noreferrer" ><FaInstagram style={{ height: "47px", width: "47px", margin : "10px" }} /></a>
-          <a href='https://www.linkedin.com/company/sai-super-pack/' target='_blank' rel="noreferrer" ><FaLinkedin style={{ height: "47px", width: "47px", margin : "10px" }} /></a>
+          <a href='https://www.facebook.com/profile.php?id=100079658515735' target='_blank' rel="noreferrer" ><FaFacebook id='fb' style={{ height: "47px", width: "47px", margin : "10px" }} /></a>
+          <a href='https://www.instagram.com' target='_blank' rel="noreferrer" ><FaInstagram id='fb' style={{ height: "47px", width: "47px", margin : "10px" }} /></a>
+          <a href='https://www.linkedin.com/company/sai-super-pack/' target='_blank' rel="noreferrer" ><FaLinkedin id='fb' style={{ height: "47px", width: "47px", margin : "10px" }} /></a>
         </div>
       </div>
     </div>
